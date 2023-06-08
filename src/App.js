1324a0750754f8cc792efb0ff3881b9e55bf54a0
God Twinkle <tnl04122001@gmail.com>
2023-06-08 16:35:32 +0700

    coverage

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 import { Header } from "./components/layout/Header";
 import { Content } from "./components/layout/Content";
 import { ProjectsProvider, SelectedProjectProvider } from "./context";
-import { SendMessage } from "./components/SendMessage";
 
 export const App = ({ darkModeDefault = false }) => {
   const [darkMode, setDarkMode] = useState(darkModeDefault);
@@ -18,7 +17,7 @@ export const App = ({ darkModeDefault = false }) => {
         >
           <Header darkMode={darkMode} setDarkMode={setDarkMode} showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
           <Content showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
-          <SendMessage />
+        
         </main>
       </ProjectsProvider>
     </SelectedProjectProvider>
