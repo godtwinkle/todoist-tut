ea8cac441103a231df1505167a1f120dd7ebb788
God Twinkle <tnl04122001@gmail.com>
2023-06-08 16:33:32 +0700

    coverage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { Header } from "./components/layout/Header";
 import { Content } from "./components/layout/Content";
 import { ProjectsProvider, SelectedProjectProvider } from "./context";
+import { SendMessage } from "./components/SendMessage";
 
 export const App = ({ darkModeDefault = false }) => {
   const [darkMode, setDarkMode] = useState(darkModeDefault);
@@ -17,6 +18,7 @@ export const App = ({ darkModeDefault = false }) => {
         >
           <Header darkMode={darkMode} setDarkMode={setDarkMode} showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
           <Content showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
+          <SendMessage />
         </main>
       </ProjectsProvider>
     </SelectedProjectProvider>
---
+++ src/components/SendMessage.js
@@ -0,0 +1,8 @@
+import React, { useEffect } from 'react';
+import {firebase} from '../firebase';
+import twilio from 'twilio';
+
+export const SendMessage = () => {
+
+    return 'hello';
+}
